671f7aa4513cfc9161d045813aa33d9ebdbac676
Tacsy <lelielru@gmail.com>
2017-05-11 11:36:24 -0400

    add header

--- calHda_GMH.py
+++ calHda_GMH.py
@@ -1,3 +1,26 @@
-#!/usr/bin/env python
+#!/usr/bin/env python 
 #-*- coding: utf-8 -*-
+
+# this python script is to calculate electronic coupling between two specific frontier orbital using GMH method.
+
+# steps
+#
+#
+#
+#
+
+# syntax:
+
+
+from sys import argv,exit
+import numpy as np
+import numpy.linalg as la
+from math import sqrt
+import argparse
+
+#script name
+script = argv[0]
+
+#parser setup
+#for help information, use ./calHda_GMH.py -h [--help]
 
