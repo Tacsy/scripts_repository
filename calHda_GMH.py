3fb4e73d0b75f77339ebbc8334a4ea96b4c0acca
Tacsy <lelielru@gmail.com>
2017-05-15 18:00:30 -0400

    add build matrix functions

--- calHda_GMH.py
+++ calHda_GMH.py
@@ -24,8 +24,8 @@ script = argv[0]
 #parser setup
 #for help information, use ./calHda_GMH.py -h [--help]
 parser = argparse.ArgumentParser()
-parser.add_argument()
-parser.add_argument()
+parser.add_argument('-d', dest = 'dipole', help = 'Dipole matrix, XYZ lower triangular form', type=str)
+parser.add_argument('-m', dest = 'MOcoef', help = 'MO coefficient', type=str)
 parser.add_argument()
 parser.add_argument()
 
@@ -78,4 +78,94 @@ def build_matrix(filename):
             full_matrix[i,j] = np.conjugate(full_matrix[j,i])
 
     return full_matrix, dim
+########################################
+
+########################################
+def build_dipo_matrix(filename):
+   
+    #open file
+    f = open(filename, 'r')
+    lines = f.readlines()
+    f.close()
+
+    #read all lower triangle matrix elements in the list
+    numlist = []
+    for line in lines:
+        linelist = line.split()
+        for num in linelist:
+            numlist.append(float(num))
+
+    #get the length of the list and get the dimension
+    tria_num = len(numlist)
+    
+    #test wheather it can be divided by 3
+    if tria_num % 3 == 0:
+        num = tria_num/3
+    else:
+        print "The input parsed is not a 3D dipole matrix, please look into it and try again"
+        exit()
+    
+    for i in range(int(sqrt(num*2)),1,-1):
+        if i*(i+1)/2.0 == num:
+            dim = i
+            break
+        else:
+            print "The input parsed is not a lower triangle matrix, please look into it and try again"
+            exit()
+   
+    #build X,Y,Z dipole matrix
+    dipX = np.matrix(np.zeros((dim,dim)))
+    dipY = np.matrix(np.zeros((dim,dim)))
+    dipZ = np.matrix(np.zeros((dim,dim)))
+
+    idx = 0
+
+    #build lower triangular matrix element
+    for i in range(dim):
+        for j in range(i+1):
+            dipX[i,j] = numlist[num*0+idx]
+            dipY[i,j] = numlist[num*1+idx]
+            dipZ[i,j] = numlist[num*2+idx]
+            idx = idx + 1
+
+    #build upper triangular matrix element
+    for i in range(dim):
+        for j in range(i+1,dim):
+            dipX[i,j] = np.conjugate(dipX[j,i])
+            dipY[i,j] = np.conjugate(dipY[j,i])
+            dipZ[i,j] = np.conjugate(dipZ[j,i])
+
+    return dipX, dipY, dipZ, dim
+########################################
+
+########################################
+def build_mo_matrix(filename):
+    
+    #open file
+    f = open(filename, 'r')
+    lines = f.readlines()
+    f.close()
+
+    #read all lower triangle matrix elements in the list
+    numlist = []
+    for line in lines:
+        linelist = line.split()
+        for num in linelist:
+            numlist.append(float(num))
+
+    #get the length of the list and get the dimension
+    num = len(numlist)
+    dim = int(sqrt(num))
+
+    #build MO matrix
+    MO = np.matrix(np.zeros((dim,dim)))
+
+    idx = 0
+    for i in range(dim):
+        for j in range(dim):
+            MO[i,j] = numlist[idx]
+            idx = idx + 1
+
+    return MO
+########################################
 
